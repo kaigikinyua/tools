ed93a0c62c34b34c9013063b8705a96e9ada5e3d
kaigikinyua <kaigikinyua@gmail.com>
2019-05-03 21:37:00 +0300

    loading settings now to get to know json

--- DirSort/Control.py
+++ DirSort/Control.py
@@ -10,12 +10,18 @@ class Control:
 		else:
 			print("Moved "+cwd+"/todos to /home/antony/Desktop")
 	"""
+	#extract extensions
 	def extension(self,filename):
 		try:
 			ext=filename.split(".")
 			return ext[1].lower()
 		except:
 			return "no extension"
+	#load settings from settings.json
+	def settings(self):
+		f=FileActions()
+		settings=f.simpleFile('settings.json','r')
+		print(settings)
 c=Control()
-x=c.extension("KINYUA.ANTONY.KAIGI")
+x=c.settings()
 print(x)
