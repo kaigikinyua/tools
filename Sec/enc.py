6a77d344df2d7141755786fab51a0c4d486388d2
kaigikinyua <kaigikinyua@gmail.com>
2019-09-05 21:34:28 +0300

    adding the rock

---
+++ Sec/enc.py
@@ -0,0 +1,14 @@
+class Security:
+	def __init__(self,data):
+		print(data)
+		x=open('keys.txt','r')
+		keys=x.readlines()
+		x.close()
+		newData=""
+		for j in range(len(data)-1):
+			for i in range(len(keys[0])-1):
+				if data[j]==keys[0][i]:
+					newData+=keys[1][i]
+		print(newData)
+
+s=Security("My name is Antony")
