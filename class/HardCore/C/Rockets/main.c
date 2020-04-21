fb78a5338b972f4615e96371dd2bca1aa156f697
kaigikinyua <kaigikinyua@gmail.com>
2020-04-21 15:27:53 +0300

    c#

---
+++ class/HardCore/C/Rockets/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc,char *argv){
+
+}
+/*module 1
+int launch(){}
+int run_diagnostics(){}
+int check_weather(){}
+*/
+
+/*
+int mechanic(){}
+int save_rocket(){}
+
+int delete_rocket_settings(){}
+
+int read_file(){}
+*/
