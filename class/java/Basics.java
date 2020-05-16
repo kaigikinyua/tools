db0d2e813ffa581ccab375f7a82c94ad3a6ce2e0
kaigikinyua <kaigikinyua@gmail.com>
2020-05-17 01:32:21 +0300

    java refresher

---
+++ class/java/Basics.java
@@ -0,0 +1,19 @@
+
+
+public class Basics{
+	public static void main(String[] args){
+		System.out.println("Hello world");
+		InputOutPut.outPut("My first static function in java");
+	}
+};
+
+
+class InputOutPut{
+	public static void outPut(String outputString){
+		System.out.println(outputString);
+	}
+}
+
+//need to have and understand
+//1.@overide
+//List and Array Strings
