1d36d176d7286f8b3d945da6fe9580d5d060a2f2
kaigikinyua <kaigikinyua@gmail.com>
2020-06-10 17:56:46 +0300

    toJSON function

--- class/Dart/Parsing/model.dart
+++ class/Dart/Parsing/model.dart
@@ -1,13 +1,24 @@
 class TodoListModel{
   List<Todo> todos;
-  TodoListModel(this.todos);
-  TodoListModel.fromJson(Map<String,dynamic> json){
-    if(json["todos"]!=null){
+  TodoListModel({this.todos});
+  TodoListModel.fromJson(List<dynamic> json){
+    if(json!=null){
      todos=new List<Todo>();
-     json["todos"].forEach((v){
+     json.forEach((v){
        todos.add(new Todo.fromJson(v));
      });
     }
+  }
+  Map<String,dynamic> toJson(){
+    Map<String,dynamic> data;
+    if(this.todos!=null){
+      todos.map((f)=>{
+        print(f)
+      });
+      //data["todos"]=this.todos.map((f)=>f.toJson()).toList();
+      //print(data["todos"]);
+    }
+    return data;
   }
 }
 
@@ -15,10 +26,17 @@ class Todo{
   String todo;
   String time;
   bool routine;
-  Todo(this.todo,this.time,this.routine);
+  Todo({this.todo,this.time,this.routine});
   Todo.fromJson(Map<String,dynamic> json){
     todo=json["todo"];
     time=json["time"];
     routine=json["routine"];
+  }
+  Map<String,dynamic> toJson(){
+    Map<String,dynamic> data;
+    data["todo"]=this.todo;
+    data["time"]=this.time;
+    data["routine"]=this.routine;
+    return data;
   }
 }
