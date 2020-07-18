e76e41038fcd7705c087ad202a75fb98d39413bf
kaigikinyua <kaigikinyua@gmail.com>
2020-07-18 23:34:30 +0300

    layout of the autosort tool

---
+++ des/auto_sort/main.py
@@ -0,0 +1,28 @@
+import sys
+from Utils.Engine import *
+
+class ProgFlow:
+    @staticmethod
+    def program_init(*args, **kwargs):
+        pass
+    @staticmethod
+    def full_auto_sort():
+        pass
+    @staticmethod
+    def managed_auto_sort():
+        pass
+    @staticmethod
+    def log_mode():
+        pass
+    @staticmethod
+    def diagnostics():
+        pass
+
+
+def appRun(*args, **kwargs):
+    pass
+
+if __name__=="__main__":
+    print("Auto sort starting....")
+    cli_args=sys.argv
+    appRun(cli_args)
