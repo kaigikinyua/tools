84a67e9f50867c70468b5200a87dc56fc74428c0
kaigikinyua <kaigikinyua@gmail.com>
2020-07-27 17:44:31 +0300

    mapped some custome C libs in Lib

---
+++ des/auto_sort/Lib/scan.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+char list_directory(char dir_path){
+    return "x";
+}
+
+int check_match(char file_name,char phrase){
+    int match=0;
+    return match;
+}
+
+int main(){
+    int error_code=0;
+    return 0;
+}
