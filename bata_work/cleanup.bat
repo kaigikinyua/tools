eccec185f3e31f2de3b5c0d89ea17fdd8a3e1e54
kaigikinyua <kaigikinyua@gmail.com>
2020-11-11 17:08:31 +0300

    commit

--- bata_work/cleanup.bat
+++ bata_work/cleanup.bat
@@ -3,3 +3,6 @@ mkdir './test/orant'
 cp -r './test/orant_prev/* ./test/orant/'
 rm -r './test/orant_prev'
 rm '/test/destination/*.*'
+
+
+''''''''''''booooooooooorrrrrriiiinnnnnnnnngggggg''''''''''''''''''''
