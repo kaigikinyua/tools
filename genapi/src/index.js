8c4d2ee63cfdd50a5872ee26ea85e5aa0820ad15
kaigikinyua <kaigikinyua@gmail.com>
2020-12-26 11:10:06 +0300

    path error solved

--- genapi/src/index.js
+++ genapi/src/index.js
@@ -1,10 +1,13 @@
 const express=require("express")
-
+const path=require("path")
 const app=express()
 
+//app.set("view engine","ejs")
+
 app.get("/",(req,res)=>{
-    res.sendFile("views/index.html")
+    res.sendFile(path.join(__dirname+"/views/index.html"))
 })
+
 
 app.listen(4000,()=>{
     console.log("GENapi running on port 4000")
