64dca3e7ae83c1f45d322d841a44b7562e1e6197
kaigikinyua <kaigikinyua@gmail.com>
2020-12-27 11:43:38 +0300

    index.js adjustements

--- genapi/src/index.js
+++ genapi/src/index.js
@@ -2,13 +2,29 @@ const express=require("express")
 const path=require("path")
 const app=express()
 
+//const DataBase=require("./modules/database")
+//import  DataBase from './modules/database';
 //app.set("view engine","ejs")
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname+"/views/index.html"))
+});
+
+app.get('/fetch/all/:database',(req,res)=>{
+    console.log(req.params.database)
+    res.end(JSON.stringify({"data":"[{'key':'value1'}]"}))
+});
+
+app.get('/fetch/filter/:database/:query',(req,res)=>{
+    var database=req.params.database;
+    var query=req.params.query
+    console.log(query)
+    res.end(JSON.stringify({"data":"['key':'fetch filter not implemented']"}))
 })
 
 
 app.listen(4000,()=>{
     console.log("GENapi running on port 4000")
 })
+
+
--- genapi/src/modules/database.js
+++ genapi/src/modules/database.js
@@ -1,12 +1,10 @@
-const path=require("path")
-const fs=require("fs")
-class DataBase{
-    database="";
-    constructor(database){this.database=database}
-    create(data){}
-    delete(key,value){}
+//const path=require("path")
+const fs=require("fs");
+/*class DataBase{
+    //database;
+    constructor(database){this.database="../Data/"+database}
     fetch_all(){
-        var f=File(this.database)
+        var f=new File(this.database)
         var data=f.readFile()
         if(data!=false){
             return data;
@@ -17,21 +15,32 @@ class DataBase{
     }
     //fetch_filter(){}
     //update(){}
+    //create(data){}
+    //delete(key,value){}
 }
-
+*/
+const DataBase=function(app,{database,query,data}){
+    app.get('fetch/all/:database',req,res=>{
+        console.log(req.params.database)
+        res.end(JSON.stringify({"data":"[{'key':'value1'}]"}))
+    });
+}
 class Files{
-    filename=""
+    //filename;data;
     constructor(filename){this.filename=filename}
     readFile(){
         var data=fs.createReadStream(this.filename,(err,data)=>{
             if(err){
-                return false
+                this.data=false
             }else{
-                return data
+                this.data=data
+                console.log(data)
             }
         })
+        return this.data;
     }
     //createFile(){}
     //deleteFile(){}
 
 }
+module.exports = { DataBase } 
