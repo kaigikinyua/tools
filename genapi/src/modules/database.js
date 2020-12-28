1fcbf2b52815c4611759c316c331003b5b0feb13
kaigikinyua <kaigikinyua@gmail.com>
2020-12-28 11:45:34 +0300

    tyding up database.js in genAPI

--- genapi/src/modules/database.js
+++ genapi/src/modules/database.js
@@ -1,30 +1,4 @@
-//const path=require("path")
-const fs=require("fs");
-/*class DataBase{
-    //database;
-    constructor(database){this.database="../Data/"+database}
-    fetch_all(){
-        var f=new File(this.database)
-        var data=f.readFile()
-        if(data!=false){
-            return data;
-        }else{
-            console.error("Error while reading from file "+this.filename)
-            return {}
-        }
-    }
-    //fetch_filter(){}
-    //update(){}
-    //create(data){}
-    //delete(key,value){}
-}
-*/
-const DataBase=function(app,{database,query,data}){
-    app.get('fetch/all/:database',req,res=>{
-        console.log(req.params.database)
-        res.end(JSON.stringify({"data":"[{'key':'value1'}]"}))
-    });
-}
+const fs=require("fs")
 class Files{
     //filename;data;
     constructor(filename){this.filename=filename}
