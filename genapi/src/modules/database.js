f42b643c631f3396d1d8f2dd007e19726d235ecf
kaigikinyua <kaigikinyua@gmail.com>
2020-12-28 12:21:11 +0300

    Data folder shift to solve for path error in genAPI

--- genapi/src/modules/database.js
+++ genapi/src/modules/database.js
@@ -1,7 +1,31 @@
 const fs=require("fs")
-class Files{
+const path=require("path")
+
+module.exports=function({filename,type,callback}){
+    console.log("Hello world")
+    fetch_all=()=>{
+        console.log("fetching all")
+        filename="/Data/"+filename
+        var read=fs.readFile(path.join(__dirname+filename),'utf-8',(err,data)=>{
+            if(err){
+                console.error(err)
+                callback(false)
+            }else{
+                console.log(data)
+                console.log("data read from "+filename)
+                callback(JSON.parse(data))
+            }
+        });
+        return {}
+    }
+    if(type=="fetch_all"){
+        return fetch_all()
+    }
+}
+
+/*class Files{
     //filename;data;
-    constructor(filename){this.filename=filename}
+    constructor(filename){this.filename="../Data/"+filename}
     readFile(){
         var data=fs.createReadStream(this.filename,(err,data)=>{
             if(err){
@@ -16,5 +40,4 @@ class Files{
     //createFile(){}
     //deleteFile(){}
 
-}
-module.exports = { DataBase } 
+}*/ 
