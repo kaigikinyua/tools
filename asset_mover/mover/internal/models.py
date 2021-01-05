4d7bbe5ac17005a3be2fe499e98d43fed1947c5c
kaigikinyua <kaigikinyua@gmail.com>
2021-01-05 10:19:24 +0300

    models update

--- asset_mover/mover/internal/models.py
+++ asset_mover/mover/internal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class UserItem(models.Model):
+    itemName=models.CharField(max_length=50)
+    
+class Owner(models.Model):
+    currentOwner=models.CharField(max_length=100)
+    itemDesc=models.CharField(max_length=50)
+    itemSN=models.CharField(max_length=200)
+    date=models.CharField(max_length=50)
+
+class PC(models.Model):
+    owner=models.CharField(max_length=50)
+    pcModel=models.CharField(max_length=50)
+    osVer=models.CharField(max_length=50)
+    ram=models.IntegerField()
+    cpu=models.CharField(max_length=50)
+    activated=models.BooleanField(default=True)
+
+class ClearedOwner(models.Model):
+    owner=models.CharField(max_length=50)
+    itemSN=models.CharField(max_length=50)
+    cleared=models.BooleanField(default=False)
+    clearedDate=models.DateField(auto_now=True, auto_now_add=True)
+    location=models.CharField(max_length=50)
+
+
--- asset_mover/mover/shops/models.py
+++ asset_mover/mover/shops/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ShopItem(models.Model):
+    itemName=models.CharField(max_length=50)
+    
+class ShopStock(models.Model):
+    currentOwner=models.CharField(max_length=100)
+    currentLocation=models.CharField(max_length=50)
+    itemDesc=models.CharField(max_length=50)
+    itemSN=models.CharField(max_length=200)
+    date=models.CharField(max_length=50)
+
+class ClearedShop(models.Model):
+    shopName=models.CharField(max_length=50)
+    itemSN=models.CharField(max_length=50)
+    cleared=models.BooleanField(default=False)
+    clearedDate=models.DateField(auto_now=True, auto_now_add=True)
+
+
