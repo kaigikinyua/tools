08b430cebb39a483404e0fc640a3f68e3d1ab682
kaigikinyua <kaigikinyua@gmail.com>
2021-01-05 11:02:25 +0300

    shopassets and shopinventory html in asset_mover

--- asset_mover/mover/shops/urls.py
+++ asset_mover/mover/shops/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 urlpatterns = [
     #shopsassets
+    path('shopsinventory/',views.shopsInventory,name="shopsinventory"),
+    path('shopassets/<str:shopname>',views.ShopAssets.as_view(),name="shopassets")
     #shopasset/<shopid>
     #receive
     #despatch
--- asset_mover/mover/shops/views.py
+++ asset_mover/mover/shops/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def shopsInventory(request):
+    return render(request,"shops/shopsinventory.html")
+
+class ShopAssets(View):
+    items=[
+        {"desc":"Datecs FP 300","sn":"AD30102011","location":"MIS"},
+        {"desc":"Hp Monitor","sn":"FBCAMCA920","location":"Shop"},
+        {"desc":"Epson LX-300","sn":"*Q4293506","location":"Arseco"}
+    ]
+    def get(self,request,shopname):
+        shopname=shopname
+        return render(request,"shops/shopassets.html",{"shopname":shopname,"items":self.items})
+    #login required
+    def post(self,request,shopname):
+        shopname=shopname
+        return render(request,"shops/shopassets.html",{"shopname":shopname,"items":self.items})
