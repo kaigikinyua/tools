bd5e369a470210ba20d5e0463b42603ee11e1d2f
kaigikinyua <kaigikinyua@gmail.com>
2021-01-05 10:31:43 +0300

    registering models in asset_mover

--- asset_mover/mover/shops/admin.py
+++ asset_mover/mover/shops/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from shops.models import *
 # Register your models here.
+admin.site.register(ShopItem)
+admin.site.register(ShopStock)
+admin.site.register(ClearedShop)
+
