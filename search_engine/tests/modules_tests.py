05421a66cc7d992b891dd5c7dbc8c3a6a47b11ed
kaigikinyua <kaigikinyua@gmail.com>
2021-01-10 23:32:33 +0300

    tests init for search

---
+++ search_engine/tests/modules_tests.py
@@ -0,0 +1,16 @@
+#import unit test
+
+class UtilitiesTest:
+    def files_test():
+        pass
+
+class TemplatesTests:
+    def webpage():
+        pass
+
+class JsonRequestTests:
+    def search():
+        pass
+
+
+
