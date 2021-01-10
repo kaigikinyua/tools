90b54646f6045b0d574006abef5cd4072f3f166b
kaigikinyua <kaigikinyua@gmail.com>
2021-01-10 22:21:10 +0300

    search engine

---
+++ search_engine/static/js/theme.js
@@ -0,0 +1,19 @@
+var lmode=null;var dmode=null;
+const colors=[
+    {mode:"dark",containers:"#303030",title:"#ddddee",font:"#efefef"},
+    {mode:"light",containers:"#whitesmoke",title:"#090909",font:"#a9a9a9"},
+]
+
+window.onload(()=>{
+     lmode=document.getElementById("lightmode")
+     dmode=document.getElementById("lightmode")
+})
+
+function change_mode({mode}){
+    var body=document.querySelector("body")
+    if(mode=="light"){
+
+    }else{
+
+    }
+}
